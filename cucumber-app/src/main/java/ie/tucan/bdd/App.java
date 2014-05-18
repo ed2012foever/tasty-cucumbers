832dc0e1d91978f85e4ce021c6c91d3811a1ce58
Ed Gohery <edgohery@yahoo.co.uk>
2014-05-18 15:46:00 +0100

    Adding print method to App class.

--- cucumber-app/src/main/java/ie/tucan/bdd/App.java
+++ cucumber-app/src/main/java/ie/tucan/bdd/App.java
@@ -8,6 +8,10 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World!" );
+    	printHelloWorld();
+    }
+
+    private static void printHelloWorld() {
+    	System.out.println("Hello World!");
     }
 }
