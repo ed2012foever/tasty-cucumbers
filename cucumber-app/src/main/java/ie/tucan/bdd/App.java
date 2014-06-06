9924cca87fec5acb1120c33764ea06574d6628e1
Ed Gohery <edgohery@yahoo.co.uk>
2014-06-06 20:20:50 +0100

    Updated pom and added basic test main.

--- cucumber-app/src/main/java/ie/tucan/bdd/App.java
+++ cucumber-app/src/main/java/ie/tucan/bdd/App.java
@@ -1,18 +1,35 @@
 package ie.tucan.bdd;
 
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.phantomjs.PhantomJSDriver;
+import org.openqa.selenium.remote.DesiredCapabilities;
+
 /**
  * Driver class for the cucumber app
  * 
  * @author Ed Gohery
  */
-public class App 
-{
-    public static void main( String[] args )
-    {
-    	printHelloWorld();
-    }
+public class App {
+	public static void main(String[] args) {
+		
+		WebDriver driver;
+		String baseUrl;
+		StringBuffer verificationErrors = new StringBuffer();
+		DesiredCapabilities dCaps;
 
-    private static void printHelloWorld() {
-    	System.out.println("Hello World!");
-    }
+		dCaps = new DesiredCapabilities();
+		dCaps.setJavascriptEnabled(true);
+		dCaps.setCapability("takesScreenshot", false);
+
+		driver = new PhantomJSDriver(dCaps);
+		baseUrl = "https://uk.yahoo.com/";
+		driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+
+		driver.get(baseUrl + "/");
+		System.out.println(driver.getPageSource());
+		driver.quit();
+	}
+
 }
